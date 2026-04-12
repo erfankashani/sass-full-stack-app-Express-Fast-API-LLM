ff33dd9c7d849b8371f90277d148c281c0bb6d4a
Erfan Kashani <erfan.kashani@cantire.com>
2026-04-12 19:16:21 -0400

    first commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,13 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        <title>Canada Trivia Generator</title>
+        <meta name="description" content="AI-powered Canada Trivia generation" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
